c3b8642bf9b2a5167f007934f97d7c5fddf351f0
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-26 23:18:11 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,6 @@
+\ // Listen for input events on the colorPicker element
+\ 
+\ // Create a function called 'updateColor' to handle input event
+\ 
+\ // Update the background-color of the colorDisplay div
+\ 
