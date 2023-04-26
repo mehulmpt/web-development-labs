0a74574ee178d196a39cfe681f8d752725b45279
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-26 21:42:36 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,8 @@
+// An array of image URLs to be used as a placeholder for the slider
+const images = [
+    'https://via.placeholder.com/938x300.png?text=1',
+    'https://via.placeholder.com/938x300.png?text=2',
+    'https://via.placeholder.com/938x300.png?text=3'
+]
+
+// Implement your slider logic here
