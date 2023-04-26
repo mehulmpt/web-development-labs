217ed8aebebb50366a979c6d3ad680d190526b9b
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-26 20:12:08 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,3 @@
+// Write a function to fetch data from the API and display it in the 'result' div
+
+// Add an event listener to the button with id 'fetch-button' to call the fetchData function on click
