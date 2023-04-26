c1662b6da3060ac6817dde947afd8a3ee1927d44
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-26 23:19:14 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,5 @@
+// Implement form submission event listener and prevent page refresh
+
+// Implement adding tasks and remove buttons to the list
+
+// Implement removing tasks from the list when remove buttons are clicked
