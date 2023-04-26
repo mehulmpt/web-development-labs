1f01a0bbfa7af14f039868c9fcec66dc7c35fcc7
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-26 23:23:22 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,11 @@
+// create snake and ladder objects
+
+// create a function to initialize the game
+
+// create a function for dice roll
+
+// create a function to move player
+
+// create a function to check win condition
+
+// create a function to check if player encounters snake or ladder
