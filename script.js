62c95abe501faea2ce4f90517488508707ab896e
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-26 22:26:52 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,2 @@
+// Attach click eventListener to your fetchData button here and fetch data from https://jsonplaceholder.typicode.com/posts
+// Display the fetched data as post titles in the 'dataContainer' div
