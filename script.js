c0ccc40f979216ffece336ead36c51ac02c4458f
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 09:48:37 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add event listeners for the search button and clear button
+
+// Implement fetchImageData_function which fetches the data from Unsplash API
+
+// Implement renderImages_function which renders the images to the results div
+
+// Implement clearResults_function which clears the input and results div
