c7021b3c1e8866ab5b36de905b06efdf9d03bacd
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 11:27:36 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,19 @@
+const products = [
+    // Add your product data here as objects in this array
+];
+
+let cart = [];
+
+function addToCart(index) {
+    // Code your function to add an item to the cart
+}
+
+function updateCartUI() {
+    // Code your function to update the UI with new cart details
+}
+
+function clearCart() {
+    // Code your function to clear the cart
+}
+
+// Bind the event listeners or call functions if needed
