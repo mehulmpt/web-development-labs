097a1ca06f99b403caf5bea5048fbcbe161d52bc
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 01:16:39 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,8 @@
+// write your code here
+function addItem() {
+    // push entered value into array and clear the input field
+}
+
+// add event listener to the button
+
+// function to update itemCount and display the list of products
