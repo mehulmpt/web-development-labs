f05e0fdde2f7e3449991609e08ee86cd8aed1606
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 01:07:54 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,17 @@
+const input = document.getElementById('name-input');
+
+input.addEventListener('focus', function () {
+    // Add your code here
+});
+
+input.addEventListener('input', function () {
+    // Add your code here
+});
+
+input.addEventListener('blur', function () {
+    // Add your code here
+});
+
+document.getElementById('my-form').addEventListener('submit', function (event) {
+    // Add your code here
+});
