d5b452fbce90af9f868b5e0a0ec75bc1bbc78889
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 08:33:57 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,6 @@
+// Write a JavaScript function 'saveDocument()' to save the content of the textarea when the 'Save' button is clicked.
+
+// Attach the 'saveDocument()' function to the 'Save' button.
+
+// Load the saved content into the textarea when the page is reloaded.
+
