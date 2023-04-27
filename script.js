29ca826dffa2f4b46c87f21d870551a9c5afb111
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 12:39:41 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,9 @@
+// Write your code here
+
+// Define the recognizeImage function
+
+// Hint: Use the FileReader API to read the image file
+
+// Hint: Use the Fetch API to make a call to the (mock) image recognition service
+
+// Hint: Update the result element to display the recognition result
