ba0c0beea065320c2f9617a64c43caa36a9be46a
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 13:10:09 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,4 @@
+// Define a function named 'speak' which will be called when the submit button is clicked
+
+// The 'speak' function should get the input text value, and use the SpeechSynthesis API to speak the text.
+// Make sure-the application only speaks when there's text in the input field.
