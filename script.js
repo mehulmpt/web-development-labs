b335395f1c3429b476ea3358c9a5ae9aa6f67253
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 01:02:55 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,5 @@
+// Implement the add, subtract, multiply, and divide functions below and attach them to the buttons
+
+// Display the result of each operation in the 'result' div
+
+// Reset the input fields after performing an operation
