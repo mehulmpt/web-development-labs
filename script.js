bd343db8e806649b0aa046f08be42d8443ccb738
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 08:22:44 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,3 @@
+// Write your code to create calendar functionality here
+// Create a calendar table, modal box, event input and save button
+// Show the modal on day cell click and save the event input to the respective day
