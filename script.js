35a799454b7533ce86042fc8a89e8df6f9172776
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 01:44:34 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,7 @@
+// Initialize the SpeechRecognition object
+
+// Set the recognition lang and interimResults properties
+
+// Add an event listener for the 'result' event
+
+// Add a click event listener for the 'start-recognition' button
