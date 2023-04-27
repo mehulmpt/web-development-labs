f47df6cc29a13e05d7fc6d402101e2d4e938bbb0
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 09:54:52 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,5 @@
+// Implement utility functions 'drawRectangle' and 'drawLine'
+
+// Attach event listeners for 'mousedown', 'mousemove', and 'mouseup' events to the canvas element
+
+// Attach event listener for 'input' event to the color picker element
