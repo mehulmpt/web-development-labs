070bbbc914f97c2f392d44814e7224cc044b5f51
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 08:38:41 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,3 @@
+// Add an event listener to the button with the ID 'btn', so that when it is clicked - background color of the div with the ID 'colorBox' will change.
+
+// Also, set the initial background color of the 'colorBox'.
