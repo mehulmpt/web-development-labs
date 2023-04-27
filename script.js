cdf83bf4b8027963cb1a75f8209f791b9372aeee
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 11:06:53 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,8 @@
+// Create a JavaScript function 'togglePanel()' here
+
+/*
+Toggle the 'hide-panel' class if the panel has it:
+  1. Get the panel element by its id.
+  2. Check if the panel has the 'hide-panel' class.
+  3. If the panel has the class, then toggle it.
+*/
