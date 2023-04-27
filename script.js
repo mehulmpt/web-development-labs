878d70b1ad819ef11933acc6819919108f66c344
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 12:09:09 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add event listener for addExpense button
+
+// Create addExpense function
+function addExpense() {
+    // Get expense description and amount
+
+    // Create a new div for expense
+
+    // Append expense to expenseList section
+}
