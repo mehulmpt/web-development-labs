2e23068abe9f4c8d29a058e67f25f513465b66d5
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 12:01:03 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,5 @@
+// Setup the webcam stream and capture button here
+
+// Load face-api.js models, detect faces, landmarks, and recognize faces
+
+// Draw bounding boxes and display descriptor values
