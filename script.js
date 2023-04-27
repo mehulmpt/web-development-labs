c36837671435132edf08eae9440cf02ddf6c9b21
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 10:45:43 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,11 @@
+// Create the required functions here
+
+// initAudioContext()
+
+// loadAudioFile(audioUrl, audioContext)
+
+// playAudioBuffer(audioBuffer, audioContext)
+
+// createAnalyser(audioContext)
+
+// drawVisualizer(analyser, canvas)
