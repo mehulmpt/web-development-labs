d36eba224963a03da053527cd611cd52eb64f648
Ubuntu <ubuntu@ip-172-31-11-5.ec2.internal>
2023-04-27 00:24:31 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,7 @@
+// write your code here
+
+// Example code
+// document.getElementById('submit').addEventListener('click', submitAnswers)
+// function submitAnswers() {
+    // Calculate score logic
+// }
