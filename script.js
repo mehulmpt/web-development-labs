961a580df33a5b1cecfdf1aaab14fc28c9a2b132
Ubuntu <ubuntu@ip-172-31-17-185.ec2.internal>
2023-04-28 23:41:34 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,7 @@
+// Create a URLSearchParams instance using the query string '?key=value&name=example'
+
+// Get the value of the 'key' parameter and log it to the console
+
+// Update the value of the 'key' parameter to 'newValue'
+
+// Convert the URLSearchParams instance back to a string and log it to the console
