0ac0459a3e23dc39595c1c234ad0ef767e2d9efc
Mehul Mohan <mehulmpt@gmail.com>
2023-04-29 04:01:14 +0530

    Add files via script

---
+++ main.js
@@ -0,0 +1,13 @@
+// Initialize canvas element
+
+// Capture and draw the current video frame on the canvas
+function captureFrame() {
+	// Add your code here
+}
+
+// Detect motion between two frames
+function detectMotion() {
+	// Add your code here
+}
+
+// Set up a loop to call captureFrame and detectMotion
