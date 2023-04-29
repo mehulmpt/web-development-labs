2938a38e4b7007615ae4eb5c045d386ba4adb11b
Ubuntu <ubuntu@ip-172-31-45-29.ec2.internal>
2023-04-29 09:27:59 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,7 @@
+// Create a canvas element with an ID of 'canvas' and set its width to 300 and height to 200.
+
+// Draw a red rectangle with a width of 50, a height of 50 and top-left coordinates at (20, 20).
+
+// Draw a blue circle with a radius of 30 and center coordinates at (100, 100).
+
+// Draw a green line from (150, 50) to (250, 150).
