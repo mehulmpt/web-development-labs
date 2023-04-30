8b0d3d90a7b3cd486fa55b71f57a78badd48c412
Ubuntu <ubuntu@ip-172-31-45-29.ec2.internal>
2023-04-30 10:48:17 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,9 @@
+// Write your code here
+
+// Step 1: Create a function named 'displayNotification'
+
+// Step 2: Add an onclick event to the button to call 'displayNotification'
+
+// Step 3: Request permission for showing notifications inside 'displayNotification'
+
+// Step 4: Create a new notification with the title 'Hello Codedamn' when permission is granted
