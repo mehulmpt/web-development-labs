68a3254098e1165b32d7b289cee75489398955b9
Ubuntu <ubuntu@ip-172-31-45-29.ec2.internal>
2023-04-30 10:28:36 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,11 @@
+// Your WebGL code goes here.
+
+// Step 1: Access the canvas element and create a WebGL context.
+
+// Step 2: Create and compile the vertex shader.
+
+// Step 3: Create and compile the fragment shader.
+
+// Step 4: Create a square mesh with vertices and colors.
+
+// Step 5: Create the animation logic for the rotating colored square.
