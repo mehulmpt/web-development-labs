8b00b9d8d74a71e2ac05a5e1de35f1e81c98a8e9
Ubuntu <ubuntu@ip-172-31-45-29.ec2.internal>
2023-04-30 11:43:06 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,3 @@
+// Write your JavaScript code here to handle click events and update the content of the paragraph element.
+
+// For the click events, use inline JavaScript or addEventListener function.
