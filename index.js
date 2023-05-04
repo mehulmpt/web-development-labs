2d544cfe437b7b8333ac8e6d6fc00bb7115c4670
Ubuntu <ubuntu@ip-172-31-81-53.ec2.internal>
2023-05-04 21:12:09 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,15 @@
+class ProductList {
+	constructor(products) {
+		this.products = products
+	}
+
+	// TODO: Your code goes here
+}
+
+const products = [
+	{ name: 'Product A', price: 100 },
+	{ name: 'Product B', price: 200 },
+	{ name: 'Product C', price: 300 },
+]
+
+// Your code goes here...
