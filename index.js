c7211c06c174b324325b7c296849fe8698bdc483
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 07:54:40 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,7 @@
+// Create targetObject with properties 'name' and 'age' here
+
+// Create deleteProperty function here
+
+// Delete 'age' property from targetObject
+
+// Export the final targetObject
