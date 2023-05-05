5e746aac1a5801e21110c6a5a6855f433e050d6f
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 09:57:58 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,5 @@
+/*
+Write your code in this file.
+Create a function 'generateRandomNumber' that takes two arguments, a minimum number, and a maximum number.
+Export the function using ESM syntax, handle the input values and make sure it doesn't throw errors.
+*/
