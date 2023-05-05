e00feba35ca6a662a6d69640e2002d60a2a00998
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 11:03:20 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,7 @@
+// Start by creating an object 'person' with properties 'name', 'age', and 'city'
+
+// Next, use object destructuring to extract the properties from 'person'
+
+// Create a function named 'extractName' that takes an object as input and returns the 'name' property using object destructuring
+
+// Create an array named 'numbers' and use array destructuring to extract the first three elements
