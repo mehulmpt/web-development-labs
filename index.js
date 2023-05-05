349dfc428a50b211025079021b95d943f88ba45f
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 10:59:50 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,7 @@
+/* Create customAdd() function */
+
+/* Handle cases when the provided arguments are of different types */
+
+/* Handle string concatenation if the provided arguments are strings */
+
+/* Handle the edge case when the provided arguments are of type boolean */
