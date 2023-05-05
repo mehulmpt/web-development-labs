65b91291b29d65648144154b60da250f4739c955
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 11:26:20 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Welcome to the JavaScript Proxies Lab!')
+
+// Start by creating your target object and handler object
+
+// Next, create a Proxy using the target object and handler object
+
+// Implement the 'get' trap in the handler object
+
+// Modify return value of a property in the 'get' trap
+
+// Use the proxy to access properties of the target object
