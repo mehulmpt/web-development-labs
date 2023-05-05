bba09b778728bfc7936543118f5f0a37662caa87
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 12:06:45 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,5 @@
+// Start by creating an array named 'numbers' containing numbers 1 through 5.
+
+// Write a 'for...of' loop that iterates through the 'numbers' array.
+
+// Calculate the sum of all the numbers in the 'numbers' array.
