16feea443397953cbee3eb61dde862b627850c60
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 12:09:35 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,5 @@
+// Start by creating the CustomError class
+
+// Next, create an instance of CustomError with a custom message
+
+// Finally, log the message property of the error object to the console
