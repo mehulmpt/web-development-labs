de01ce5d7fb7835fa93a7c8e984be7b7da1df756
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 07:59:53 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('JavaScript Date Object Lab')
+
+//Create a new Date object called `currentDate` and log the current date and time
+
+//Create a function `formatDate` that takes a Date object and returns a formatted string in the format 'dd-mm-yyyy'
+
+//Create a function `getDayName` that takes a Date object and returns the day of the week as a string
+
+//Export the `formatDate` function using ESM syntax
