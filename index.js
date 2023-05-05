d28bc7034ee5a98da587554b04d823ebd6b7b747
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 10:42:33 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,5 @@
+// Create your Person class here
+
+// Create an instance of the Person class and store it in 'personInstance'
+
+// Use JavaScript reflection to obtain the constructor name and store it in 'constructorName'
