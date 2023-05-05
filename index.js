9d1b87c1623ff5f88dd43ffd7917ebc6a6dfa88a
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 12:20:44 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,7 @@
+// Start by creating the numbers array containing numbers 1 through 5
+
+// Now, create the tenNumbers array by concatenating the numbers array with a new array containing numbers from 6 to 10
+
+// Write the sumArray function that sums elements of the input array
+
+// Call the sumArray function with tenNumbers array as argument and assign the result to sum variable
