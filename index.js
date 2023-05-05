20249d649b62fca8b767fb24b678126b70f13d16
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 10:34:18 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,15 @@
+class Singleton {
+	// Your implementation goes here
+}
+
+export default Singleton
+
+// instance creation
+// const instance1 = Singleton.getInstance();
+// const instance2 = Singleton.getInstance();
+
+// set message
+// instance1.setMessage('Hello World');
+
+// get message
+// console.log(instance2.getMessage());
