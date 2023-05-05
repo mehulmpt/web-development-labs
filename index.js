1293ee7c200ff65716af2176fd2678596181575c
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 11:02:35 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,7 @@
+// Start by importing the events module and creating an instance of EventEmitter
+
+// Next, create the eventHandler function
+
+// Now, attach the eventHandler as a listener to the data_received event
+
+// Finally, emit the data_received event with the payload 'Hello, EventEmitter!'
