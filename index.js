a59c3898d1b8ea082834302544ead2447d0a193b
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 11:41:16 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+// Create an Express server
+const app = express()
+
+// TODO: Implement middleware and endpoints
+
+// Listen on port 1337
+app.listen(1337, () => console.log('Listening on port 1337'))
