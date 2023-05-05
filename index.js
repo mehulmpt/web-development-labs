30a74d845fc5ade601128d5c0e14a2cfd29d27ca
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 09:42:23 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * Implement your validation functions here.
+ * Don't forget to export them at the end.
+ */
+
+// Example function for your reference
+function exampleFunction() {
+	return 'Hello world!'
+}
