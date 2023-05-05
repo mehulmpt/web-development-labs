7b924a41ec90b88161ee7a74896bcb80a7245b0b
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 08:15:49 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,3 @@
+// Write a function named 'parseFloating' that takes a single string argument and returns the parsed floating-point value using 'Number.parseFloat()'.
+
+// Remember to export the function using ESM syntax.
