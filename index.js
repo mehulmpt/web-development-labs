6dba8de6bcd1936f6146c81d7fa6c2401b50df40
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 10:38:32 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Hello World')
+
+// Step 1: Create the car object
+
+// Step 2: Create the getOwnPropertyNames function
+
+// Step 3: Call the function and store the result
+
+// Step 4: Export the objects using ESM syntax
