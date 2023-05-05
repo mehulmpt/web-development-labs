e280670158becdaded5d49b7f51ec7f6a63790dc
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 09:21:38 +0000

    Add files via script

---
+++ sortingAlgorithms.js
@@ -0,0 +1,15 @@
+export function bubbleSort(array) {
+	// Your code here
+}
+
+export function selectionSort(array) {
+	// Your code here
+}
+
+export function insertionSort(array) {
+	// Your code here
+}
+
+export function quickSort(array) {
+	// Your code here
+}
