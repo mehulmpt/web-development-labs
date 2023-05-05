34639475d289d6f13195391be19192717b123d85
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 12:32:37 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,5 @@
+// Import the findSum function from the 'sum.js' file
+
+// Call the findSum function with two arguments
+
+// Add a comment about the function call
---
+++ sum.js
@@ -0,0 +1,1 @@
+// Create and export a findSum function
