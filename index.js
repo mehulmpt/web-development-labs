486e295ac873a7a448a49240f171da8a2755afb2
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 09:21:03 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,15 @@
+export function reverseString() {
+	// Your code here
+}
+
+export function capitalizeFirstLetters() {
+	// Your code here
+}
+
+export function findLongestWord() {
+	// Your code here
+}
+
+export function wordCount() {
+	// Your code here
+}
