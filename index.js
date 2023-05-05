43ede2df0e1d2eb9e32809cf164b812a70d3c34e
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 10:17:55 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,7 @@
+export function sumArray(arr) {
+	// Implement the sumArray function using lodash sum()
+}
+
+export function squareArray(arr) {
+	// Implement the squareArray function using lodash map()
+}
