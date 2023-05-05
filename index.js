0af7d4aa7bb46b41ad37b878aa0883d96e536144
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 11:22:22 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Starting Decorator Pattern Lab...')
+
+// Create Decorator class here
+
+// Implement Component class here
+
+// Add ConcreteComponent class here
+
+// Create ConcreteDecoratorA class here
