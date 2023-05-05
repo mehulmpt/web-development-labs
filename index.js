8f200d5059927af00917c77a005406311adcaf6e
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 10:52:37 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+// Create an Express app
+const app = express()
+
+// Your task: Create a GET endpoint at /api/messages
+
+// Start the server on port 1337
+app.listen(1337, () => console.log('Server is listening on port 1337'))
