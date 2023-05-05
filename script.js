c45c9e78a6f15b5448a67d016ff76f9a36fb6cdc
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 07:45:34 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,5 @@
+// Start by importing the 'child_process' module
+
+// Spawn a new child process executing the appropriate command
+
+// Handle the output of the child process and any errors that may occur
