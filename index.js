0044a4d610ba0c0d0f64232eb8eee05e189e8aba
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 07:29:42 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,11 @@
+import process from 'process'
+
+console.log('TODO: Complete the challenges')
+
+// Challenge 1: Create myBuffer
+
+// Challenge 2: Convert myBuffer to a string and store it in resultString
+
+// Challenge 3: Print resultString
+
+// Challenge 4: Create a new encoded buffer and store it in encodedBuffer
