7a8d0dc1ced582b25f96758b2fb7bc3ccf7c1069
Ubuntu <ubuntu@ip-172-31-81-53.ec2.internal>
2023-05-05 00:12:53 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+ * Step 1: Create 'allPositive' function using array.prototype.every()
+ * Step 2: Export 'allPositive' function
+ * Step 3: Create 'startsWithCapital' function using array.prototype.every()
+ * Step 4: Export 'startsWithCapital' function
+ */
