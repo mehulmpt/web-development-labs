55aacfabf21b0d02b6015f8660cb5737f8e07334
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 10:46:55 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,10 @@
+import http from 'http'
+
+// Create an HTTP server listening on port 1337
+
+// Import the createRevocableProxy function from revocableProxy.js
+
+// Example target object
+const target = {}
+
+// Create a revocable proxy
---
+++ revocableProxy.js
@@ -0,0 +1,1 @@
+// Write the createRevocableProxy function here, which takes target and handler objects as inputs, and returns a revocable proxy.
