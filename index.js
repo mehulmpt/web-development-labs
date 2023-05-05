a58ed835333f025790b09b80e52e313cd782a02e
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 07:43:53 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,10 @@
+const car = { color: 'red', brand: 'Toyota', price: 20000 }
+
+const numbers = [1, 2, 3]
+
+let x = 10
+let y = 20
+
+const items = ['apple']
+
+console.log('Initial values:', car, numbers, x, y, items)
