1c275d88ceedd4b8c0b7fcb903730cefdc43c2cd
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 08:05:10 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,7 @@
+// Create a set named 'mySet' and add numbers 1, 2, and 3 to it.
+
+// Define the 'union' function that takes two sets as input.
+
+// Define the 'intersection' function that takes two sets as input.
+
+// Export the 'union' and 'intersection' functions using ESM syntax
