9ea674a4464cab2c782425812ca7af9eee23e5ab
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 12:06:34 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+import { exec, spawn } from 'child_process'
+
+// Create child process using exec method and run a shell command
+
+// Print the output of the shell command in the console
+
+// Handle errors during child process execution
+
+// Create child process using spawn method and run a command with arguments
