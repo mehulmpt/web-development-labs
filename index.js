20cccd8f04422e74900cb42cac5d972e261144f5
Ubuntu <ubuntu@ip-172-31-39-208.ec2.internal>
2023-05-05 11:55:34 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log(
+	'Welcome to the Symbol.toStringTag lab! Start by creating a class named Person.'
+)
+
+// create the Person class here
+
+// instantiate an object 'person'
+
+// log the result of the toString() method on the 'person' object
+
+// Export the 'person' object as the default export
