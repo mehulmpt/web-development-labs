783e563303493f2f8998d1bc1865096d2b71eaf7
Mehul Mohan <mehulmpt@gmail.com>
2023-05-09 04:15:40 +0530

    Add files via script

---
+++ index.js
@@ -0,0 +1,18 @@
+function multiplyn (num1, num2) {
+  retur num1 * num2;
+}
+
+const n1 = 5;
+const n2 = 7;
+
+if (n1 > 0
+  const result = multiply(n1, n2);
+  console.log('Result:', result);
+}
+
+const user = {
+  name: 'John',
+  age; 28
+};
+
+console.log(user.totalPoints);
