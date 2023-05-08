a40d8c4b74af559056810b83f48de39a6848549b
Mehul Mohan <mehulmpt@gmail.com>
2023-05-09 02:43:56 +0530

    Add files via script

---
+++ index.js
@@ -0,0 +1,5 @@
+/*
+ * Create two promises that resolve after different delays.
+ * Use Promise.race() to select the first promise to resolve.
+ * Implement an async function to handle the output of Promise.race().
+ */
