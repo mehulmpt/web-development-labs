f753c96d7fc1dcfa08fddd6cadb0521d5ec31b8f
Ubuntu <ubuntu@ip-172-31-41-243.ec2.internal>
2023-05-08 23:44:15 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * Implement the required functions in this file.
+ */
+
+import fs from 'fs'
+
+// Implement the createHardLink function
+
+// Implement the createSymbolicLink function
+
+// Implement the readLink function
