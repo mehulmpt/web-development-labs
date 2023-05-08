aa361cf889264aa50f7b10f888e66bcac5509806
Mehul Mohan <mehulmpt@gmail.com>
2023-05-09 03:45:20 +0530

    Add files via script

---
+++ index.js
@@ -0,0 +1,7 @@
+// Start by creating an array named todos
+
+// Next, create a function named addTodo
+
+// Then, create a function named getTodos
+
+// Finally, export both the functions using ESM syntax
