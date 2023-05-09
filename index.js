189524af934d4002295a449970048d5728143fa6
Ubuntu <ubuntu@ip-172-31-81-116.ec2.internal>
2023-05-09 16:51:02 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,11 @@
+// Import lodash and perform the required operations on the initialObject to create the modifiedObject
+
+const initialObject = {
+	1: 'one',
+	2: 'two',
+	3: 'three',
+}
+
+// Your code here
+
+// Export the modifiedObject
