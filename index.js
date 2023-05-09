6c68ae6ff2a0ead4a5dade28fffc6a1a1a9b11e9
Ubuntu <ubuntu@ip-172-31-81-116.ec2.internal>
2023-05-09 18:04:37 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,7 @@
+import './solution'
+
+console.log('sortedIndexBy')
+
+export default function sortedIndexBy(userData, orderBy) {
+	// Implement your solution here
+}
---
+++ solution.js
@@ -0,0 +1,4 @@
+/*
+  Add your solution implementation in this file
+  And import it in the 'index.js' file to test
+*/
