f8ec3a444957a943b9d7201567b97189732008ab
Ubuntu <ubuntu@ip-172-31-41-243.ec2.internal>
2023-05-09 00:53:10 +0000

    Add files via script

---
+++ server.js
@@ -0,0 +1,7 @@
+import http from 'http'
+
+// TODO: Create a server and bind it to port 1337.
+
+// TODO: Respond with 'Hello, World!' to every incoming request.
+
+// TODO: Export the server as default export from this file.
