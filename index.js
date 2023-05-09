db898924df19d85108bd629526379684e23558ad
Ubuntu <ubuntu@ip-172-31-41-243.ec2.internal>
2023-05-09 01:51:42 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,18 @@
+import fs from 'fs'
+import zlib from 'zlib'
+
+// Write your code here
+
+/*
+  You should create these two functions:
+
+  async function compressFile(inputFilename, compressedFilename) {
+    // Your code here
+  }
+
+  async function decompressFile(compressedFilename, outputFilename) {
+    // Your code here
+  }
+*/
+
+// Make sure to export both functions
