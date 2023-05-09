14f38eee312bd77fcf07ac2db53b5b09424025b5
Ubuntu <ubuntu@ip-172-31-81-116.ec2.internal>
2023-05-09 16:55:25 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,11 @@
+// Start coding here by importing the 'os' module and defining the required functions.
+
+// getUserInfo()
+
+// getPlatform()
+
+// getTotalMemory()
+
+// getFreeMemory()
+
+// Export the functions
