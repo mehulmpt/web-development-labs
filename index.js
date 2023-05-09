d613b6b6438ee30b8702c5c1e07b5d48a94a72f7
Ubuntu <ubuntu@ip-172-31-41-243.ec2.internal>
2023-05-09 00:58:55 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,4 @@
+// Import the necessary module and create a new EventEmitter instance
+// Register a listener for the 'data' event
+// Emit a 'data' event with a value
+// Remove the registered listener for the 'data' event
