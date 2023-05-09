206cf7f9805e1d6afe9ae4f63d8045be5ce21409
Ubuntu <ubuntu@ip-172-31-41-243.ec2.internal>
2023-05-09 00:00:06 +0000

    Add files via script

---
+++ child.js
@@ -0,0 +1,5 @@
+// #2: Spawn communication
+
+// #3: Catch parent process message
+
+// #4: Send back the result of the calculation
---
+++ index.js
@@ -0,0 +1,9 @@
+import http from 'http'
+
+// #1: Start an HTTP server
+
+// #2: Create child process and communication
+
+// #3: Send message to child process
+
+// #4: Catch response from child process
