452b2cde28e1ee644d5f805b7bee6955b07ce5cf
Ubuntu <ubuntu@ip-172-31-81-116.ec2.internal>
2023-05-09 17:06:28 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,7 @@
+// Your code goes here
+
+// Install lodash library and import as _
+
+// Create getMax function that takes an array and returns the maximum value
+
+// Export getMax function as default
