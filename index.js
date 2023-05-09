04ca85b3b5ac7e3bad66224b15810c55cfc9a05a
Ubuntu <ubuntu@ip-172-31-81-116.ec2.internal>
2023-05-09 19:12:25 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Welcome to the Day.js lab')
+
+// Import Day.js library
+
+// Create currentDate variable
+
+// Set currentDate to the start of the current hour using dayjs.startOf()
+
+// Export `currentDate` as a default export
