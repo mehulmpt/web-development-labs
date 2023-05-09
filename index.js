25877cfc8e8b1cc4ad3b8be45d5942ed8c902a36
Ubuntu <ubuntu@ip-172-31-81-116.ec2.internal>
2023-05-09 20:27:00 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,7 @@
+// Import the url module here
+
+// Find and export the 'resolve' function from the url module
+
+// Use the 'resolve' function to merge two relative URLs
+
+// Export the merged URL
