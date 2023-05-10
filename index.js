26211a176bf51b41c11e8dfa70a91df5029002b8
Ubuntu <ubuntu@ip-172-31-21-236.ec2.internal>
2023-05-10 10:07:34 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,11 @@
+// Use ESM imports and work with the fs and stream modules to accomplish the tasks.
+
+// Create a Read stream and export it as readStream.
+
+// Create a Write stream and export it as writeStream.
+
+// Create a transform stream that converts the text to uppercase and export it as uppercaseTransformStream.
+
+// Use stream.pipeline() to pipe readStream to uppercaseTransformStream and then writeStream.
+
+// Handle errors in stream.pipeline().
