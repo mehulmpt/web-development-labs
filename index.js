d83d5fb494cc0075e9db2d72675e5570b8e191d2
Ubuntu <ubuntu@ip-172-31-21-236.ec2.internal>
2023-05-10 09:46:08 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+// Import child_process
+
+// Create a child process using spawn()
+
+// Add a data event listener to stdout
+
+// Add a data event listener to stderr
+
+// Add an exit event listener to child
