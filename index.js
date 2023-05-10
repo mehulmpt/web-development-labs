6940c5721909594ed7ac5e5b304075cf9fbe0ece
Ubuntu <ubuntu@ip-172-31-21-236.ec2.internal>
2023-05-10 09:56:31 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,11 @@
+// Import required modules
+
+// create your server here
+
+// listen to the given port and host
+
+// handle your GET request here
+
+// export your server
+
+export default server
