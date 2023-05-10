8fbbe9e72229f7427ce8a7661cd1d5c2302ee19b
Ubuntu <ubuntu@ip-172-31-21-236.ec2.internal>
2023-05-10 21:13:19 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+// Import lodash library
+import _ from 'lodash'
+
+// Your tasks:
+// 1. Understand the usage of _.last() function in lodash.
+// 2. Implement a custom getLast() function that mimics the behavior of _.last().
+// 3. Test your custom getLast() function with different test cases.
+
+// Feel free to play with lodash _.last() function below to understand its usage.
