2c9ef7ea1185c2d996a664d9cce84a642820d541
Ubuntu <ubuntu@ip-172-31-95-56.ec2.internal>
2023-05-11 09:11:35 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+import { Writable } from 'stream'
+
+class CustomWritableStream extends Writable {
+	// Implement _write and _final methods
+}
+
+export default CustomWritableStream
+
+// Test your writable stream here
