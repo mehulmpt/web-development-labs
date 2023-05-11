ef318904c09d74e34274bd8f0dec94955fbfe235
Ubuntu <ubuntu@ip-172-31-95-56.ec2.internal>
2023-05-11 16:06:02 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,21 @@
+// write your code here
+import fs from 'fs'
+import { Transform } from 'stream'
+
+// Create a read stream for the input file
+// TODO
+
+// Process the data using a custom transform stream
+// TODO
+
+// Write the output to a new file using a write stream
+// TODO
+
+// Use stream.pipeline() to combine the streams you created
+// TODO
+
+// Export the pipeline function for testing
+// TODO
+
+// Proper error handling using stream.finished()
+// TODO
