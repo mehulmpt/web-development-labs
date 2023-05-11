b6b9504c50cf790cb8608691ad383cfbf3a37362
Ubuntu <ubuntu@ip-172-31-95-56.ec2.internal>
2023-05-11 15:17:43 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,5 @@
+import MyReadable from './myReadable.js'
+
+// Listen for data and end events on the custom stream.
+
+// Define and export getFinalOutput() function here
---
+++ myReadable.js
@@ -0,0 +1,7 @@
+import { Readable } from 'stream'
+
+class MyReadable extends Readable {
+	// Implement _read method here
+}
+
+// Export your custom stream instance here
