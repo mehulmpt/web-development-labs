0889b7422d6d5a872ad3f6f9bd5bcfd0a0cb6c83
Ubuntu <ubuntu@ip-172-31-21-28.ec2.internal>
2023-05-12 00:09:50 +0000

    Add files via script

---
+++ StreamEventEmitter.js
@@ -0,0 +1,1 @@
+// Implement StreamEventEmitter class here
---
+++ index.js
@@ -0,0 +1,3 @@
+import StreamEventEmitter from './StreamEventEmitter.js'
+
+// Use StreamEventEmitter class to create read and write streams, and handle custom events
