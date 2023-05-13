c9d3ec53c2ad0189491d7ab0da4d62cab5bd57c4
Ubuntu <ubuntu@ip-172-31-21-28.ec2.internal>
2023-05-13 22:19:36 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,7 @@
+// Task 1: Declare an async function called fetchData
+
+// Task 2: Call the fetchData function and handle promise resolution
+
+// Task 3: Use the await keyword inside an async function to wait for fetchData's promise
+
+// Task 4: Combine multiple async functions using Promise.all() and Promise.race()
