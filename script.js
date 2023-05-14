2130f87b4aaa065b6cf66f7993bd572014249430
Ubuntu <ubuntu@ip-172-31-21-28.ec2.internal>
2023-05-14 11:23:25 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,24 @@
+// JSON data is provided here. Write your code to parse, manipulate and display the data.
+
+const jsonData = `{
+  "users": [
+    {
+      "id": 1,
+      "name": "John Doe",
+      "email": "john.doe@example.com",
+      "age": 28
+    },
+    {
+      "id": 2,
+      "name": "Jane Smith",
+      "email": "jane.smith@example.com",
+      "age": 35
+    },
+    {
+      "id": 3,
+      "name": "Mike Brown",
+      "email": "mike.brown@example.com",
+      "age": 40
+    }
+  ]
+}`
