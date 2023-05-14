6f4dcf2b20e81e1ca0258cc59abdf42da04b7706
Ubuntu <ubuntu@ip-172-31-21-28.ec2.internal>
2023-05-14 18:05:41 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,11 @@
+const { ApolloServer, gql } = require('apollo-server');
+const typeDefs = gql\`
+    // Write your schema here
+\`;
+
+const resolvers = {
+    // Create your resolvers here
+};
+
+// Setup the Apollo server
+
