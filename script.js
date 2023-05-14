e271d8bb349f7eeb30409ae52c828c566cc9e590
Ubuntu <ubuntu@ip-172-31-21-28.ec2.internal>
2023-05-14 18:52:51 +0000

    Add files via script

---
+++ script.js
@@ -0,0 +1,8 @@
+// Implement your isPalindrome function here
+function isPalindrome(string) {
+	// ...
+}
+
+document.getElementById('checkPalindrome').addEventListener('click', () => {
+	// Handle the button click and update the result paragraph
+})
