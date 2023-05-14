838c89e71b972ded5ba9d202051bf1958bdcd8c6
Ubuntu <ubuntu@ip-172-31-21-28.ec2.internal>
2023-05-14 14:21:24 +0000

    Add files via script

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+// Implement API route for receiving form submission data
+
+app.listen(3000, () => {
+	console.log('Server listening on port 3000')
+})
---
+++ script.js
@@ -0,0 +1,1 @@
+// Write your JavaScript code for handling form submission
