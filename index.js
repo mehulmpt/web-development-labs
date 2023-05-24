1b92b4c5056ac98f4c136e5362a8fbbe9de67d3b
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 03:48:28 +0530

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Enhance Array prototype with a snail method
+ * @param {number} rowsCount
+ * @param {number} colsCount
+ * @return {Array<Array<number>>}
+ */
+Array.prototype.snail = function (rowsCount, colsCount) {}
+
+export default Array.prototype.snail
