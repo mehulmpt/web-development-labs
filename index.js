5680276cc6dd015b45f04bfd798ea2403836bfe5
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 03:38:52 +0530

    Add files via script

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {any} obj
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+export const checkIfInstanceOf = function (obj, classFunction) {}
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
