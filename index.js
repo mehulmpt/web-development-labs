5bc2d138ddd7eeee660a42b069ff9e4ad78adc42
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 03:20:01 +0530

    Add files via script

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @return {Generator<number>}
+ */
+export var fibGenerator = function* () {}
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
