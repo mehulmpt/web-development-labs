8d9b75b23dbc3791d2df27560fda7c09e2789adf
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 04:28:28 +0530

    Add files via script

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+export const once = function (fn) {
+	return function (...args) {}
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
