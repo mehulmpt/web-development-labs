2715526c4228dbc3a75274bd1f75667bfe5074ba
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 03:24:39 +0530

    Add files via script

---
+++ index.js
@@ -0,0 +1,8 @@
+export const createCounter = function (init) {}
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
