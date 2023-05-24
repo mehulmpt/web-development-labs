0ab1d8574d3ea479cacdbf177bf1906e38e2d88d
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 04:37:05 +0530

    Add files via script

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+export const throttle = function (fn, t) {
+	return function (...args) {}
+}
+
+/**
+ * const throttled = throttle(console.log, 100);
+ * throttled("log"); // logged immediately.
+ * throttled("log"); // logged at t=100ms.
+ */
