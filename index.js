4ec188cf67d055c7cea3029a86d9cf91802b0348
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 04:28:16 +0530

    Add files via script

---
+++ index.js
@@ -0,0 +1,13 @@
+// ESM export for the function
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+export const inorderTraversal = function* (arr) {}
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
