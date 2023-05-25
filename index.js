0db34329ff3ddd1cf5dce73321a73e3c1b0162bd
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 10:17:52 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+export const floodFill = function (image, sr, sc, color) {}
