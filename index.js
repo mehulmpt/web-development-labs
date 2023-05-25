2c9c92732050778379b9885d0118cb77e0681d0b
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 11:38:39 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,10 @@
+// Import necessary modules here if needed
+
+/**
+ * @param {any} obj
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+export const checkIfInstanceOf = function (obj, classFunction) {
+	// Add your implementation here
+}
