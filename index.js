012b6a83311c54295291c2bebdd27b770fdbaeb8
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 12:46:56 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,22 @@
+class MyHashSet {
+	constructor() {}
+
+	/**
+	 * @param {number} key
+	 * @return {void}
+	 */
+	add(key) {}
+
+	/**
+	 * @param {number} key
+	 * @return {void}
+	 */
+	remove(key) {}
+
+	/**
+	 * @param {number} key
+	 * @return {boolean}
+	 */
+	contains(key) {}
+}
+export default MyHashSet
