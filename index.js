f0037e6b8eef4bf1d6bed050ccd70367e1948935
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 10:18:37 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+const matrixReshape = function (mat, r, c) {}
+
+export default matrixReshape
