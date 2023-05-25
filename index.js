2a7fecc36ce59162192e1c01f8ad0f1ce6fc5f18
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 12:24:23 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} timeSeries
+ * @param {number} duration
+ * @return {number}
+ */
+const findPoisonedDuration = function (timeSeries, duration) {}
+
+export default findPoisonedDuration
