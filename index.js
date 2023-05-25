34872879e3b46acaf3186ccd95b4ca06a4acd97b
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 12:01:49 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+const compose = function (functions) {
+	return function (x) {}
+}
+
+export default compose
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
