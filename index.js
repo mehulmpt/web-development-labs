5519eb6083af16472e5877bd02427b156a0b2c40
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 11:56:21 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} rowsCount
+ * @param {number} colsCount
+ * @return {Array<Array<number>>}
+ */
+Array.prototype.snail = function(rowsCount, colsCount) {
+
+}
+
+export { Array.prototype.snail };
+
+/**
+ * const arr = [1,2,3,4];
+ * arr.snail(1,4); // [[1,2,3,4]]
+ */
