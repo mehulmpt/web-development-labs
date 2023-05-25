8480b62c07dd37757f585c4eecbd65552216f80d
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 15:27:47 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[][]} items1
+ * @param {number[][]} items2
+ * @return {number[][]}
+ */
+const mergeSimilarItems = function (items1, items2) {}
+
+export default mergeSimilarItems
