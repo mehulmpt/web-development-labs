886014ef12141e2e03e6b32a97bb21bbb117823a
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 12:03:37 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+export function* inorderTraversal(arr) {}
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
