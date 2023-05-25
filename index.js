e6b27dbd54e971618559a4272cc8f582d700cfd2
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 10:00:59 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+import { intersection } from 'index.js'
+
+// Intersection of Two Arrays function
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+export function intersection(nums1, nums2) {}
