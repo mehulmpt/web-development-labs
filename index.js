53050b5585c0a634872889dd61dfd2feaa720dc9
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 15:04:49 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[][]} ranges
+ * @param {number} left
+ * @param {number} right
+ * @return {boolean}
+ */
+const isCovered = (ranges, left, right) => {}
+
+export default isCovered
