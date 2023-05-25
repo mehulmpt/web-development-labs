a1b3075927e9cdd9836b9cd5aa7d6d1db8558b7e
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 15:57:36 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+export function maximumValue(strs) {}
+
+/**
+ * @param {string} string
+ * @return {boolean}
+ */
+export function isDigitsOnly(string) {}
