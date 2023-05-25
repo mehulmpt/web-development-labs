3665bb733f5538ee34bc3a5e27cf922f1bd16924
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 16:01:22 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * @param {string[]} words
+ * @param {string} target
+ * @param {number} startIndex
+ * @return {number}
+ */
+export const closetTarget = function (words, target, startIndex) {}
