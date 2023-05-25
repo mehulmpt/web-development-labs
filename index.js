60aeee6e2c0961d0b29c856bfd08cafc7d6972f3
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 13:07:27 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+// Add your implementation of findSpecialInteger function here
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+const findSpecialInteger = function (arr) {}
+
+// Export the findSpecialInteger function using ESM
+export default findSpecialInteger
