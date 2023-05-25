f42d5d7ed98fd3702da4fffa9e83d61a43c58896
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 14:32:58 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ */
+export class OrderedStream {
+	constructor(n) {}
+
+	/**
+	 * @param {number} idKey
+	 * @param {string} value
+	 * @return {string[]}
+	 */
+	insert(idKey, value) {}
+}
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * const obj = new OrderedStream(n)
+ * const param_1 = obj.insert(idKey,value)
+ */
