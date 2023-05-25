b78561c3bc709120103484c8089cac583c45c448
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 13:04:55 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,12 @@
+// Importing necessary libraries
+import { createServer } from 'http'
+
+// Your code
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
+ */
+const relativeSortArray = function (arr1, arr2) {}
+
+export { relativeSortArray }
