bbce8fff7b6f8824e02b3ffd2cab853ce3e517a5
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 12:22:41 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ */
+class NumArray {
+	constructor(nums) {}
+
+	/**
+	 * @param {number} left
+	 * @param {number} right
+	 * @return {number}
+	 */
+	sumRange(left, right) {}
+}
+
+export default NumArray
