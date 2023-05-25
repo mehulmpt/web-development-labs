2e67d3b803fe54b6632d3e483a720513b2a8f831
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 10:35:44 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,24 @@
+class MyHashMap {
+	constructor() {}
+
+	/**
+	 * @param {number} key
+	 * @param {number} value
+	 * @return {void}
+	 */
+	put(key, value) {}
+
+	/**
+	 * @param {number} key
+	 * @return {number}
+	 */
+	get(key) {}
+
+	/**
+	 * @param {number} key
+	 * @return {void}
+	 */
+	remove(key) {}
+}
+
+export default MyHashMap
