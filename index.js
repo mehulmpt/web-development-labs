b293c54c4921e9f3cab8582b9ab01538284e48a6
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 10:54:03 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} rows
+ * @param {number} cols
+ * @param {number} rCenter
+ * @param {number} cCenter
+ * @return {number[][]}
+ */
+export const allCellsDistOrder = function (rows, cols, rCenter, cCenter) {}
