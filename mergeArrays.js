4230a82620abd8619697bb7c2557e26afde965f1
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 09:46:52 +0000

    Add files via script

---
+++ mergeArrays.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+export function merge(nums1, m, nums2, n) {}
