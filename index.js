def2e9d69a6586758707a05aca0f22bc0681aa14
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 14:32:35 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+const countGoodTriplets = function (arr, a, b, c) {}
+
+export default countGoodTriplets
