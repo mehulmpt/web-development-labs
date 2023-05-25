b7c3154aa7c4cb36015ebee1b18e86a1df80f3d9
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 12:45:11 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} aliceSizes
+ * @param {number[]} bobSizes
+ * @return {number[]}
+ */
+const fairCandySwap = function (aliceSizes, bobSizes) {}
+
+export default fairCandySwap
