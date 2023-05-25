532d50a4fb11c51aa0c09e77c4d64f5e573bfc7f
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 14:37:17 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function numIdenticalPairs(nums) {
+	// Your code here
+}
+
+// Make sure to export numIdenticalPairs function
+export { numIdenticalPairs }
