c9cbad814857396330ee2ddf0891f41b2b2c9cda
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 14:33:19 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} arr
+ * @param {number} m
+ * @param {number} k
+ * @return {boolean}
+ */
+const containsPattern = function (arr, m, k) {}
+
+export default containsPattern
