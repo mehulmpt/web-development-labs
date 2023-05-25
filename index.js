7eb09f9fe29c1252b341fc32fb5fc696611463db
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 09:46:09 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,23 @@
+class ArrayWrapper {
+	constructor(nums) {
+		this.nums = nums
+	}
+
+	valueOf() {
+		// your implementation
+	}
+
+	toString() {
+		// your implementation
+	}
+}
+
+export default ArrayWrapper
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
