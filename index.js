445821601239eac7a3f240d4e353ae8cbad150d1
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 10:02:28 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,8 @@
+import { nextGreaterElement } from './index.js'
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+export const nextGreaterElement = function (nums1, nums2) {}
