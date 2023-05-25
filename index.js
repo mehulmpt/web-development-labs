a0e10d2272b17fa4cebc0b1f3ad201047575b599
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 09:44:29 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @return {Function}
+ */
+export const createHelloWorld = function () {
+	return function (...args) {}
+}
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
