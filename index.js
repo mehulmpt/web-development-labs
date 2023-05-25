2043f1adcb0b3c106aa944685bcf1418ba289cc7
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 15:28:32 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} initialEnergy
+ * @param {number} initialExperience
+ * @param {number[]} energy
+ * @param {number[]} experience
+ * @return {number}
+ */
+export default function minNumberOfHours(
+	initialEnergy,
+	initialExperience,
+	energy,
+	experience
+) {}
