97250b401c5cab9defaaf614a339416c0c432c83
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 14:33:16 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+/**
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+export const countMatches = function (items, ruleKey, ruleValue) {}
