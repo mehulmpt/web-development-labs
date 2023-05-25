d70902a3ea888588455eabea988c35d3da82c75b
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 13:35:48 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[][]} brackets
+ * @param {number} income
+ * @return {number}
+ */
+const calculateTax = (brackets, income) => {
+	// Your code here
+}
+
+export default calculateTax
