a60a68939064317aa3dc34f1b709926988835513
Mehul Mohan <mehulmpt@gmail.com>
2023-05-25 11:41:08 +0000

    Add files via script

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} millis
+ */
+export async function sleep(millis) {}
+
+/**
+ * Usage example:
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // expected output: 100
+ */
