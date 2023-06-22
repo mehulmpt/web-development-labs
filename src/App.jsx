bf5f10ee57aa487fa54280913ddb7f47e6800301
Mehul Mohan <mehulmpt@gmail.com>
2023-06-23 05:20:28 +0530

    Add files via script

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+	// Edit this component
+	return (
+		<>
+			<h1 data-testid="count"></h1>
+			<button data-testid="increment"></button>
+			<button data-testid="decrement"></button>
+		</>
+	)
+}
+
+export default App
