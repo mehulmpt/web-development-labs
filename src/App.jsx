b8f410a8b1cd3997b8d369fe9c198834a50d5234
Mehul Mohan <mehulmpt@gmail.com>
2023-06-23 04:21:20 +0530

    Add files via script

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+	// Edit this component
+	return (
+		<div>
+			<input data-testid="input-id" type="text" />
+		</div>
+	)
+}
+
+export default App
