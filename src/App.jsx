2962de094a4aaa8df94192293d1c9ed9354f2786
Mehul Mohan <mehulmpt@gmail.com>
2023-06-23 05:10:57 +0530

    Add files via script

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+	// Edit this component
+	return (
+		<div>
+			<form>
+				<input data-testid="input-id" name="input" type="number" />
+				<label for="input">°C</label>
+			</form>
+			<p data-testid="output">{/* Write the string here */}</p>
+		</div>
+	)
+}
+
+export default App
