c76dba7f66ab1849d6f8ad977ebce83853a22c04
Mehul Mohan <mehulmpt@gmail.com>
2023-06-23 05:32:56 +0530

    Add files via script

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+	// Edit this component
+	return (
+		<div>
+			<input data-testid="input-id" />
+			<button data-testid="button-id">Add a todo</button>
+			<ul data-testid="ul-id">{/* Return todos here */}</ul>
+		</div>
+	)
+}
+
+export default App
