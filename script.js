23dae7e6b7047b750acb989e81eeef07d8bd42d2
Pranav <pranav.techiegeek@gmail.com>
2023-09-22 13:06:44 +0530

    ready

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// write your generateColorPalette function here
+/**
+ * Create a 6 digit hexCode for the background color
+ */
+function generateRandomHexCode() {
+	// Make sure that the hexValue created for each color of RGB contains to digit hex code instead of one
+}
+
+/**
+ * Creates 5 DOM Nodes with different background colors
+ */
+function generateColorPalette() {}
