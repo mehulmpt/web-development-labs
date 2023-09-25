70aac6e918aa4565e4d7d7a0c63b18ead5147c45
Pranav <pranav.techiegeek@gmail.com>
2023-09-25 17:19:29 +0530

    ready

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 // Attach click eventListener to your fetchData button here and fetch data from https://jsonplaceholder.typicode.com/posts
-// Display the fetched data as post titles in the 'dataContainer' div
+
+// Display the fetched data as post titles in the 'postTitles' ul list
