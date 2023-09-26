6ee61c9cde7690f67adf72f3a054593653c44551
Pranav <pranav.techiegeek@gmail.com>
2023-09-26 17:23:10 +0530

    rady

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// add 'click' event listener to #sendRequest
+/**
+ * Returns a currency value if the given currency code exists
+ * @param {FormDataEvent} event - form submit event
+ */
+async function getCurrency(event) {
+	// Fetch Data from API
+	// API URL - https://api.exchangerate-api.com/v4/latest/USD
+	// Verify if the passed Country Code Exists
+	// If exists - return the value of the currency
+	// Else - show currency code entered is wrong
+}
