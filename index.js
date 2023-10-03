208bca06b728fbe81b4d44a637d2c5f83b2a1fcd
Pranav <pranav.techiegeek@gmail.com>
2023-10-03 14:12:01 +0530

    ready

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-import http from "http";
+// import the net module (Using ESM Import)
 
-// Create a HTTP Server using the Router Function from 'routes.js'
+// create a server variable using "createServer" and export it
+
+// On successfully connection with a client write 'Hello Net!' string to client
+
+// listen to the server on port 1337
--- routes.js
+++
@@ -1,6 +0,0 @@
-export function router(req, res) {
-	// Write your Routing Logic Here
-	// If URL path is `/hello` send "Hello, World!"
-	// If URL Path is `/bye` send "Goodbye, World!"
-	// If any other URL Path is encountered send "Not found"
-}
