4c61a08702d99c5ad85d4c1c5c8a28c393a99f57
Pranav <pranav.techiegeek@gmail.com>
2023-10-05 16:53:38 +0530

    ready

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Write your code for each challenge here
+// Challenge #1: Create two variables "num1" and "num2" and assign a valid numbers to them
+
+// Challenge #2: Create a variable sum and assign the sum of num1 and num2 to sum variable
+
+// Challenge #3: Create a variable "product" and assign the product of num1 and num2 to product variable
+
+// Challenge #4: Create two variables "flag1" and "flag2" and assign a valid boolean value to them
+
+// Challenge #5: Create a variable "combinedFlag" which holds the logical and value of "flag1" and "flag2" variables
