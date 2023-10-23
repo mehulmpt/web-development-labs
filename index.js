838a2212c5148367e82cbd3182706f9faf2b013a
Pranav <pranav.techiegeek@gmail.com>
2023-10-23 15:35:24 +0530

    ready

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
  * @return {number}
  */
 export function singleNumber(nums) {}
+
+// Sample Tests
+
+console.log(singleNumber([1]));
+console.log(singleNumber([4, 1, 2, 1, 2]));
+console.log(singleNumber([2, 2, 1]));
