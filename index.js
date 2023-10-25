f7d63bb978e77b84dd087b178778386af0a259f3
Pranav <pranav.techiegeek@gmail.com>
2023-10-25 21:28:46 +0530

    ready

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-console.log(
-	'Start the JavaScript Numbers Lab. Follow the instructions in the lab description.'
-)
+// Declare num1 & num2 variables and assign values to them
+
+// Declare result variable and assign the sum of num1 and num2 to result
+
+// Create a addNumber function that takes two arguments and returns the sum of them
+
+// IMPORTANT: Make sure to export all the functions and variable that you've declared using ESM Syntax
