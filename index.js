08fd418911c75e75a466a3d7d058e65f1136614f
Pranav <pranav.techiegeek@gmail.com>
2023-11-07 17:35:57 +0530

    ready

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Create the greet arrow function here and export it as default.
 
 // Sample Tests for your greet function
-console.log(greet("Neo"));
-console.log(greet("Trinity"));
-console.log(greet("Morpheus"));
+console.log(greet("Neo")); // should return "Hello, Neo!"
+console.log(greet("Trinity")); // should return "Hello, Trinity!"
+console.log(greet("Morpheus")); // should return "Hello, Morpheus!"
