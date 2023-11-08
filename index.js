fa3f452e2c75418dd8aaa07b38978ee9d8276154
Pranav <pranav.techiegeek@gmail.com>
2023-11-08 14:40:57 +0530

    ready

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 /*
  * Create your functions here
  */
+
+console.log(calcCircumference(5)); // Should output approximately 31.41592653589793
+console.log(calcCircumference(7.25)); // Should output approximately 45.553093477052
+
+console.log(calcArea(3.5)); // Should output approximately 38.48451000647496
+console.log(calcArea(10)); // Should output approximately 314.1592653589793
