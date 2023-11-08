0ac9d7b3465a598e26d338d2c647ea207c37c84b
Pranav <pranav.techiegeek@gmail.com>
2023-11-08 13:58:34 +0530

    ready

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // Create your sum function here.
 
-// Make sure to export your function
+console.log(sum(1, 2, 3, 4, 5)); // returns 15
+console.log(sum(2, 342, 54, 2, 51)); // returns 451
+console.log(sum(1, 12, 34, 2, 42, 12)); // returns 103
