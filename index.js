5dd370998a66d1a405804693d39038f33a6c82cf
Pranav <pranav.techiegeek@gmail.com>
2023-11-08 14:28:52 +0530

    ready;

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
-export const missingNumber = function (nums) {}
+export const missingNumber = function (nums) {};
+
+console.log(missingNumber([3, 0, 1]));
+console.log(missingNumber([0, 1]));
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
