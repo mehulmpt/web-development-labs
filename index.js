04fedb42cc80c48b92e24451b364f1a2490d81fc
Pranav <pranav.techiegeek@gmail.com>
2023-11-08 14:03:56 +0530

    ready

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export const capitaliseWord = (word) => {
 	// Write your code here
-}
+};
+
+capitaliseWord("hello"); // Output: 'Hello'
+capitaliseWord("mom"); // Output: 'Mom'
+capitaliseWord("dAD"); // Output: 'DAD'
