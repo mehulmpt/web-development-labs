6fc2cfaf1261d56afc2dae70d80f5edb9aad1648
Pranav <pranav.techiegeek@gmail.com>
2023-11-13 13:52:49 +0530

    ready

--- index.js
+++ index.js
@@ -4,6 +4,11 @@
  * @param {number} d
  * @return {number}
  */
-const findTheDistanceValue = function (arr1, arr2, d) {}
+const findTheDistanceValue = function (arr1, arr2, d) {};
 
-export { findTheDistanceValue }
+export { findTheDistanceValue };
+
+// Sample Tests (use run button to see logs)
+console.log(findTheDistanceValue([4, 5, 8], [1, 2, 3], 3)); // Expected Output: 1
+console.log(findTheDistanceValue([1, 4, 5, 6], [10, 12, 15], 5)); // Expected Output: 2
+console.log(findTheDistanceValue([2, 3, 4], [3, 4, 5], 1)); // Expected Output: 0
