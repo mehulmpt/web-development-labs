09398fa19a2be73c031636fdeca812e1790c55e6
Pranav <pranav.techiegeek@gmail.com>
2023-11-17 22:06:07 +0530

    ready

--- index.js
+++ index.js
@@ -8,5 +8,13 @@
 
 // Define 'arr'
 
+// Click on the 'Run' button to see the values logged to the console
+console.log({
+	num,
+	str,
+	bool,
+	arr,
+});
+
 // The code below this line is used for tests - don't change it
 export { num, str, bool, arr };
