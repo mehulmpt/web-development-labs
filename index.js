015a9c11ccbb999ff24a12f9f553aab9db3da93f
Pranav <pranav.techiegeek@gmail.com>
2023-12-13 16:38:05 +0530

    ready

--- index.js
+++ index.js
@@ -2,4 +2,9 @@
  * @param {number[]} nums
  * @return {number}
  */
-export const dominantIndex = (nums) => {}
+export const dominantIndex = (nums) => {};
+
+// Sample Tests
+console.log(`Dominant Index of [3, 6, 1, 0] is ${dominantIndex([3, 6, 1, 0])}`);
+console.log(`Dominant Index of [1, 2, 3, 4] is ${dominantIndex([1, 2, 3, 4])}`);
+console.log(`Dominant Index of [0, 0, 3, 1] is ${dominantIndex([0, 0, 3, 1])}`);
