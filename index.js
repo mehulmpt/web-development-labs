1386c64429c4a06cd8dc9d1984919055fbc4d475
Pranav <pranav.techiegeek@gmail.com>
2024-01-16 14:02:41 +0530

    ready

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-import { exec, execFile } from 'child_process'
+import { exec, execFile } from "child_process";
 
 // Implement execCommand function
 
 // Implement execFileCommand function
+
+// Sample Tests
+// You can run the file using the "Run" button to see the outputs of your functions
+
+console.log(await execCommand("ls"));
+
+console.log(await execFileCommand("ls"));
+console.log(await execFileCommand("ls", ["-la"]));
